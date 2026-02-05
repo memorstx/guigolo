b0ca975fb2f91c7a60909b4526e668e85231872e
memo <memorstx@gmail.com>
2026-02-05 11:45:52 -0600

    feat: add redirects for social media profiles to enhance referral tracking

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+  return [
+    {
+      source: '/go/figma',
+      destination: '/?utm_source=figma&utm_medium=profile&utm_campaign=profile_referral',
+      permanent: false,
+    },
+    {
+      source: '/go/github',
+      destination: '/?utm_source=github&utm_medium=profile&utm_campaign=profile_referral',
+      permanent: false,
+    },
+    {
+      source: '/go/behance',
+      destination: '/?utm_source=behance&utm_medium=profile&utm_campaign=profile_referral',
+      permanent: false,
+    },
+    {
+      source: '/go/linkedin',
+      destination: '/?utm_source=linkedin&utm_medium=profile&utm_campaign=profile_referral',
+      permanent: false,
+    },
+    {
+      source: '/go/instagram',
+      destination: '/?utm_source=instagram&utm_medium=bio&utm_campaign=portfolio_navigation',
+      permanent: false,
+    },
+  ]
+}
 };
+
+
 
 export default nextConfig;
