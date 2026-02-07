cbdca8e4116ff1dffd311f614e111fe3d88c26af
memo <memorstx@gmail.com>
2026-02-06 20:22:01 -0600

    feat: campaign profile_referral updated

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,56 @@ const nextConfig: NextConfig = {
         destination: '/?utm_source=indeed&utm_medium=bio&utm_campaign=job_search_navigation',
         permanent: false,
       },
+      {
+        source: '/go/x',
+        destination: '/?utm_source=twitter&utm_medium=bio&utm_campaign=social_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/email',
+        destination: '/?utm_source=email&utm_medium=bio&utm_campaign=contact_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/resume',
+        destination: '/?utm_source=resume&utm_medium=bio&utm_campaign=career_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/facebook',
+        destination: '/?utm_source=facebook&utm_medium=bio&utm_campaign=social_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/youtube',
+        destination: '/?utm_source=youtube&utm_medium=bio&utm_campaign=entertainment_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/gitlab',
+        destination: '/?utm_source=gitlab&utm_medium=bio&utm_campaign=development_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/dribbble',
+        destination: '/?utm_source=dribbble&utm_medium=bio&utm_campaign=design_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/onlyfans',
+        destination: '/?utm_source=onlyfans&utm_medium=bio&utm_campaign=social_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/uxcel',
+        destination: '/?utm_source=uxcel&utm_medium=bio&utm_campaign=design_navigation',
+        permanent: false,
+      },
+      {
+        source: '/go/platzi',
+        destination: '/?utm_source=platzi&utm_medium=bio&utm_campaign=education_navigation',
+        permanent: false,
+      }
     ]
   },
   async headers() {
